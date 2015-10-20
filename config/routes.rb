226ef0a2fc32d371d0bdd6fff4761cf28b45042f
jamesyu <zetaentranceserious@gmail.com>
2015-10-20 12:00:55 +0800

    Home page and show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
+  get '/home', to: 'videos#index'
+  get '/videos/:id', to: 'videos#show', as: 'video'
+  # resources :videos, only: [:show]
+  # get 'videos(/:action(/:id))', controller: 'videos'
+  # get ':controller(/:action(/:id))'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Video.create(title: 'Monk', description: 'A movie', small_cover_url: 'monk.jpg', large_cover_url: '/tmp/monk_large.jpg')
