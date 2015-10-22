72426e9e12ab0566aa856b2d7b8266b976611fe5
jamesyu <zetaentranceserious@gmail.com>
2015-10-22 14:36:18 +0800

    Show recent videos and reset db

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
+    @recent_videos = @category.recent_videos
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-3.times do
-  Video.create!(title: 'The Fierce Wife', description: 'The Fierce Wife', small_cover_url: '/tmp/the_fierce_wife.jpg', large_cover_url: '/tmp/the_fierce_wife_large.jpg', category_id: 1)
-end
+commedies = Category.create(name: "TV Commedies")
+dramas = Category.create(name: "TV Dramas")
+reality = Category.create(name: "Reality TV")
+
+Video.create!(title: 'The Fierce Wife', description: 'The Fierce Wife', small_cover_url: '/tmp/the_fierce_wife.jpg', large_cover_url: '/tmp/the_fierce_wife_large.jpg', category: commedies)
+
+Video.create!(title: 'Black and White', description: 'Black and White', small_cover_url: '/tmp/black_and_white.jpg', large_cover_url: '/tmp/black_and_white_large.jpg', category: commedies)
+
+Video.create!(title: 'In Time With You', description: 'In Time With You', small_cover_url: '/tmp/in_time_with_you.jpg', large_cover_url: '/tmp/in_time_with_you_large.jpg', category: commedies)
+
+Video.create!(title: 'Monk', description: 'Monk', small_cover_url: '/tmp/monk.jpg', large_cover_url: '/tmp/monk_large.jpg', category: commedies)
+
+Video.create!(title: 'Monk', description: 'Monk', small_cover_url: '/tmp/monk.jpg', large_cover_url: '/tmp/monk_large.jpg', category: dramas)
+
+Video.create!(title: 'Futurama', description: 'Futurama', small_cover_url: '/tmp/futurama.jpg', large_cover_url: '/tmp/futurama_large.jpg', category: dramas)
+
+Video.create!(title: 'The Fierce Wife', description: 'The Fierce Wife', small_cover_url: '/tmp/the_fierce_wife.jpg', large_cover_url: '/tmp/the_fierce_wife_large.jpg', category: commedies)
+
+Video.create!(title: 'Black and White', description: 'Black and White', small_cover_url: '/tmp/black_and_white.jpg', large_cover_url: '/tmp/black_and_white_large.jpg', category: commedies)
+
+Video.create!(title: 'In Time With You', description: 'In Time With You', small_cover_url: '/tmp/in_time_with_you.jpg', large_cover_url: '/tmp/in_time_with_you_large.jpg', category: commedies)
+
+Video.create!(title: 'Monk', description: 'Monk', small_cover_url: '/tmp/monk.jpg', large_cover_url: '/tmp/monk_large.jpg', category: commedies)
+
+Video.create!(title: 'Monk', description: 'Monk', small_cover_url: '/tmp/monk.jpg', large_cover_url: '/tmp/monk_large.jpg', category: dramas)
+
+Video.create!(title: 'Futurama', description: 'Futurama', small_cover_url: '/tmp/futurama.jpg', large_cover_url: '/tmp/futurama_large.jpg', category: dramas)
