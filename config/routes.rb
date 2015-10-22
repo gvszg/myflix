907563517dd5a057fd9d4147f8d522a43badc691
jamesyu <zetaentranceserious@gmail.com>
2015-10-22 16:34:35 +0800

    Implement static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
+  get '/', to: 'front#home'
   get '/home', to: 'videos#index'
+  get '/register', to: 'users#new'
+  get '/sign_in', to: 'sessions#new'
+  post '/sign_in', to: 'sessions#create'
+  resources :users, only: [:create]
   resources :videos, only: [:show] do
     collection do 
       get 'search', to: 'videos#search'
