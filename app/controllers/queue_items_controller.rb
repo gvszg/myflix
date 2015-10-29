d61cdf64e36c79d13a3fe046526448ef38907af3
jamesyu <zetaentranceserious@gmail.com>
2015-10-29 11:27:23 +0800

    Refector QueueItems Controller by moving detail code to private method

--- app/controllers/queue_items_controller.rb
+++ app/controllers/queue_items_controller.rb
@@ -7,7 +7,21 @@ class QueueItemsController < ApplicationController
 
   def create
     video = Video.find(params[:video_id])
-    QueueItem.create(video: video, user: current_user, position: current_user.queue_items.count + 1) unless current_user.queue_items.map(&:video).include?(video)
+    queue_video(video)
     redirect_to my_queue_path
+  end
+
+  private
+
+  def queue_video(video)
+    QueueItem.create(video: video, user: current_user, position: new_queue_item_position) unless current_user_queued_video?(video)
+  end
+
+  def new_queue_item_position
+    current_user.queue_items.count + 1
+  end
+
+  def current_user_queued_video?(video)
+    current_user.queue_items.map(&:video).include?(video)
   end
 end
