43e84bc9a16dd32d11e1aff73a4a0f70cc286fb6
jamesyu <zetaentranceserious@gmail.com>
2015-11-11 13:59:09 +0800

    Refactor QueueItem by setting @review

--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -8,12 +8,10 @@ class QueueItem < ActiveRecord::Base
   validates_numericality_of :position, { only_integer: true }
   
   def rating
-    review = Review.find_by(user_id: user.id, video_id: video.id)
     review.rating if review
   end
 
   def rating=(new_rating)
-    review = Review.find_by(user_id: user.id, video_id: video.id)
     if review
       review.update_column(:rating, new_rating)
     else
@@ -24,5 +22,11 @@ class QueueItem < ActiveRecord::Base
 
   def category_name
     category.name
+  end
+
+  private
+
+  def review
+    @review ||= Review.find_by(user_id: user.id, video_id: video.id)
   end
 end
