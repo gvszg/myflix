a10a4fb4d150e747c21876d5161f63c9856fa160
jamesyu <zetaentranceserious@gmail.com>
2015-11-11 11:28:17 +0800

    Add select_tag to queue_items index template

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,5 +1,5 @@
 module VideosHelper
-  def render_options_for_select
-    options_for_select([5, 4, 3, 2, 1].map { |number| [pluralize(number, "Star"), number] })
+  def render_options_for_select(selected=nil)
+    options_for_select([5, 4, 3, 2, 1].map { |number| [pluralize(number, "Star"), number] }, selected)
   end
 end
