60de20df4710f4219af36e820ac535c484dda75b
jamesyu <zetaentranceserious@gmail.com>
2015-12-05 12:31:28 +0800

    Modifier email setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,10 +20,12 @@ Myflix::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
+  config.action_mailer.default_url_options = { host: 'stagingmyflix.herokuapp.com'}
+
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'enigmatic-garden-9697.herokuapp.com',
+    domain:               'stagingmyflix.herokuapp.com',
     user_name:            ENV['GMAIL_USERNAME'],
     password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
