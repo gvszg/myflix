37becd78bb2da1f4e0daf33dc5066c1e9f865673
jamesyu <zetaentranceserious@gmail.com>
2015-12-05 16:53:38 +0800

    Implement InvitationsController new action and view page

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151130010712) do
+ActiveRecord::Schema.define(version: 20151205083939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "invitations", force: true do |t|
+    t.string   "recipient_name"
+    t.string   "recipient_email"
+    t.text     "message"
+    t.integer  "inviter_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
