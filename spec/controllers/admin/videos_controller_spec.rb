78d04c3a026f9108aa9c51d16e08939d4bb5d03b
jamesyu <zetaentranceserious@gmail.com>
2015-12-14 09:06:34 +0800

    Refactor by Brandon's comments

--- spec/controllers/admin/videos_controller_spec.rb
+++ spec/controllers/admin/videos_controller_spec.rb
@@ -13,8 +13,7 @@ describe Admin::VideosController do
     it "sets the @video to a new video" do
       set_current_admin
       get :new
-      expect(assigns(:video)).to be_instance_of Video
-      expect(assigns(:video)).to be_new_record
+      expect(assigns(:video)).to be_a_new Video    
     end
 
     it "sets flash error message" do
