d0385d20725bbae84e2f31b643e0ebb720b5128c
jamesyu <zetaentranceserious@gmail.com>
2015-12-20 18:46:25 +0800

    Implement initialize, successful?, error_message methods in StripeWrapper

--- spec/models/stripe_wrapper_spec.rb
+++ spec/models/stripe_wrapper_spec.rb
@@ -14,15 +14,50 @@ describe StripeWrapper do
             :cvc => "314"
           }
         ).id
-
         response = StripeWrapper::Charge.create(
           amount: 999,
           source: token,
           description: "a valid charge"
         )
+        expect(response).to be_successful
+      end
 
-        expect(response.amount).to eq(999)
-        expect(response.currency).to eq("usd")
+      it "makes a card declined charge", :vcr do
+        Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+
+        token = Stripe::Token.create(
+          :card => {
+            :number => "4000000000000002",
+            :exp_month => 12,
+            :exp_year => 2016,
+            :cvc => "314"
+          }
+        ).id
+        response = StripeWrapper::Charge.create(
+          amount: 999,
+          source: token,
+          description: "a valid charge"
+        )
+        expect(response).not_to be_successful
+      end
+
+      it "returns the error message for declined charge", :vcr do
+        Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+
+        token = Stripe::Token.create(
+          :card => {
+            :number => "4000000000000002",
+            :exp_month => 12,
+            :exp_year => 2016,
+            :cvc => "314"
+          }
+        ).id
+        response = StripeWrapper::Charge.create(
+          amount: 999,
+          source: token,
+          description: "a valid charge"
+        )
+        expect(response.error_message).to eq("Your card was declined.")       
       end
     end
   end
