47a2191825f215acf7dead42714b6cabc48cb008
jamesyu <zetaentranceserious@gmail.com>
2016-01-18 16:24:26 +0800

    Video rating stars style modified

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -2,4 +2,8 @@ module VideosHelper
   def render_options_for_select(selected=nil)
     options_for_select([5, 4, 3, 2, 1].map { |number| [pluralize(number, "Star"), number] }, selected)
   end
+
+  def rating_star
+    image_tag("/tmp/rating_map.png")
+  end
 end
