b8b40311351834e748f82fb7726a43c6bcc07c1b
jamesyu <zetaentranceserious@gmail.com>
2016-01-18 20:24:33 +0800

    Modified video rating for stars

--- app/decorators/video_decorator.rb
+++ app/decorators/video_decorator.rb
@@ -1,9 +1,9 @@
 class VideoDecorator < Draper::Decorator
   delegate_all
 
-  def rating
-    object.rating.present? ? "#{object.rating}/5.0" : "N/A"
-  end
+  # def rating
+  #   object.rating.present? ? "#{object.rating}/5.0" : "N/A"
+  # end
 
   def watch_video(watch_params)
     if watch_params
